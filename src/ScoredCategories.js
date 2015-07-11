d21f38b1b9df74dde758d24640efd2e98a7a38a9
Helder <he7d3r@gmail.com>
2015-07-11 20:51:59 -0300

    Proof of concept

---
+++ src/ScoredCategories.js
@@ -0,0 +1,94 @@
+/**
+ * Adds a table to category pages showing the pages with the top 10 revert scores
+ * @author: Helder (https://github.com/he7d3r)
+ * @license: CC BY-SA 3.0 <https://creativecommons.org/licenses/by-sa/3.0/>
+ */
+( function ( mw, $ ) {
+	'use strict';
+	var pages,
+		model = 'reverted',
+		scoreName = model + 'Score',
+		max = 10;
+	function showTable ( pages ) {
+		var i, page, $row, score,
+			$table = $( '<table><tbody><tr><th>Score</th><th>Page</th></tr></tbody></table>' )
+				.addClass( 'wikitable sortable' ),
+			$tbody = $table.find( 'tbody' );
+		for ( i = 0; i < pages.length; i++ ) {
+      		page = pages[i];
+			score = ( 100 * page[ scoreName ] ).toFixed(0);
+			$row = $( '<tr>' )
+				.append(
+					$( '<td>' ).text( score + '%' ),
+					$( '<td>' ).append(
+						$( '<a>' )
+							.attr( 'href', mw.util.getUrl( page.title, {
+								diff: page.revisions[0].revid
+							} ) )
+							.text( page.title )
+					)
+				);
+			$tbody.append( $row );
+		}
+		$( '#mw-content-text' ).prepend(
+			$table.tablesorter()
+		);
+	}
+	function getTopScores( data ) {
+		$.each( pages, function( i, page ) {
+			var score = data[ page.revisions[0].revid ][ model ];
+			if ( !score || score.error ) {
+				// return null;
+				page[ scoreName ] = 0;
+			} else {
+				page[ scoreName ] = score.probability['true'];
+			}
+		} );
+		pages = pages.sort( function( a, b ){
+			return b[ scoreName ] - a[ scoreName ];
+		} ).slice( 0, max );
+		showTable( pages );
+	}
+
+	function getScores( data ) {
+		var revids;
+    pages = data.query.pages;
+		revids = $.map( pages, function ( page ) {
+			return page.revisions[0].revid;
+		} );
+		$.ajax( {
+			url: '//ores.wmflabs.org/scores/' + mw.config.get( 'wgDBname' ) + '/',
+			data: {
+				models: 'reverted',
+				// TODO: Prevent this URL from having more than 2000 characters
+				revids: revids.join( '|' )
+			},
+			dataType: 'jsonp'
+		} )
+		.done( getTopScores );
+	}
+
+	function getCategoryMembers() {
+		var api = new mw.Api();
+		api.get( {
+			prop: 'revisions',
+			rvprop: 'ids',
+			generator: 'categorymembers',
+			gcmtitle: mw.config.get( 'wgPageName' ),
+			formatversion: 2,
+			// FIXME: add continuation!
+			gcmlimit: 150
+		} )
+		.done( getScores );
+	}
+
+	if ( mw.config.get( 'wgNamespaceNumber' ) === 14 &&
+			mw.config.get( 'wgAction' ) === 'view'
+	) {
+		$.when(
+			mw.loader.using( [ 'mediawiki.api', 'mediawiki.util', 'jquery.tablesorter' ] ),
+			$.ready
+		).then( getCategoryMembers );
+	}
+
+}( mediaWiki, jQuery ) );
